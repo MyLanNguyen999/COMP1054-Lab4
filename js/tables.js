c552cd39320c3d4ab61a32f80ead88dee64068a0
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-04-01 19:58:38 -0400

    updated html

--- js/tables.js
+++ js/tables.js
@@ -1,2 +1,3 @@
 // STEP 11: Call to tablesorter script here
-new Tablesort(document.getElementById("NHLData"));
+new Tablesort(document.getElementById("NHLData1"));
+new Tablesort(document.getElementById("NHLData2"));
